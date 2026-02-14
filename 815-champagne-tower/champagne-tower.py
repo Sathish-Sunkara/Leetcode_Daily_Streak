0ce977eb6920de8e5126ec873491911061b69733
Sathish-Sunkara <113529531+Sathish-Sunkara@users.noreply.github.com>
2026-02-14 21:25:03 +0530

    Time: 85 ms (52.98%) | Memory: 19.7 MB (20.76%) - LeetSync

--- 815-champagne-tower/champagne-tower.py
+++ 815-champagne-tower/champagne-tower.py
@@ -3,9 +3,9 @@ class Solution:
         dp = [[0 for j in range(row+1)] for row in range(query_row+2)]
         dp[0][0] = poured
 
-        for i in range(query_row+2) :
-            if i == query_row :
-                return 1 if dp[i][query_glass] > 1 else   dp[i][query_glass]
+        for i in range(query_row+1) :
+            # if i == query_row :
+            #     return 1 if dp[i][query_glass] > 1 else   dp[i][query_glass]
 
             for j in range(i+1) :
                 if dp[i][j] > 1 :
